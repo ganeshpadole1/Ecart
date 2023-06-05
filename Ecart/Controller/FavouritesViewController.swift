1ca51f1fc8e745ad0a7cefa673a1140d8b629bc4
ganeshpadole <ganeshpadole55@gmail.com>
2023-06-05 18:52:16 +0530

    - added products and favourites view controller

--- Ecart/Controller/FavouritesViewController.swift
+++ Ecart/Controller/FavouritesViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  FavouritesViewController.swift
 //  Ecart
 //
 //  Created by Ganesh on 05/06/23.
@@ -7,13 +7,13 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class FavouritesViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
         // Do any additional setup after loading the view.
     }
-
+    
 
 }
-
---
+++ Ecart/Controller/ProductsViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ProductsViewController.swift
+//  Ecart
+//
+//  Created by Ganesh on 05/06/23.
+//
+
+import UIKit
+
+class ProductsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
